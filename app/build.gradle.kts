986586ea843eaa02abd026ee6903b970f1ff2a2f
mgioiosa <mariopio.gioiosa@lastminute.com>
2020-12-24 16:23:17 +0100

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val compileKotlin by tasks.getting(KotlinCompile::class) {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+}
+
+val compileTestKotlin by tasks.getting(KotlinCompile::class) {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+}
+
+plugins {
+    kotlin("jvm") version "1.4.21"
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("javax.mail:javax.mail-api:1.6.2")
+
+    testImplementation(platform("org.junit:junit-bom:5.7.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
